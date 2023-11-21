b75f2674a45b15fc37d609eb68cc629022205b54
viktechsolutions <viktechsolutions@yandex.ru>
2023-11-21 17:09:49 +0300

    All tasks are done

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import {createElement} from './utils.js';
+
 import App from './app.js';
 import Store from './store.js';
 
 const store = new Store({
-  list: [
-    {code: 1, title: 'Название элемента'},
-    {code: 2, title: 'Некий объект'},
-    {code: 3, title: 'Заголовок'},
-    {code: 4, title: 'Очень длинное название элемента из семи слов'},
-    {code: 5, title: 'Запись'},
-    {code: 6, title: 'Шестая запись'},
-    {code: 7, title: 'Седьмая запись'},
-  ]
+    list: [
+        {code: 1, title: 'Название элемента', clicked: 0},
+        {code: 2, title: 'Некий объект', clicked: 0},
+        {code: 3, title: 'Заголовок', clicked: 0},
+        {code: 4, title: 'Очень длинное название элемента из семи слов', clicked: 0},
+        {code: 5, title: 'Запись', clicked: 0},
+        {code: 6, title: 'Шестая запись', clicked: 0},
+        {code: 7, title: 'Седьмая запись', clicked: 0},
+    ]
 });
 
 const root = createRoot(document.getElementById('root'));
 
 store.subscribe(() => {
-  root.render(<App store={store}/>);
+    root.render(<App store={store}/>);
 });
 
 // Первый рендер приложения
--- src/store.js
+++ src/store.js
@@ -2,78 +2,103 @@
  * Хранилище состояния приложения
  */
 class Store {
-  constructor(initState = {}) {
-    this.state = initState;
-    this.listeners = []; // Слушатели изменений состояния
-  }
-
-  /**
-   * Подписка слушателя на изменения состояния
-   * @param listener {Function}
-   * @returns {Function} Функция отписки
-   */
-  subscribe(listener) {
-    this.listeners.push(listener);
-    // Возвращается функция для удаления добавленного слушателя
-    return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
+    constructor(initState = {}) {
+        this.state = initState;
+        this.listeners = []; // Слушатели изменений состояния
+        this.usedCodes = new Set(initState.list.map(item => item.code)); // Использованные коды
     }
-  }
 
-  /**
-   * Выбор состояния
-   * @returns {Object}
-   */
-  getState() {
-    return this.state;
-  }
+    /**
+     * Подписка слушателя на изменения состояния
+     * @param listener {Function}
+     * @returns {Function} Функция отписки
+     */
+    subscribe(listener) {
+        this.listeners.push(listener);
 
-  /**
-   * Установка состояния
-   * @param newState {Object}
-   */
-  setState(newState) {
-    this.state = newState;
-    // Вызываем всех слушателей
-    for (const listener of this.listeners) listener();
-  }
-
-  /**
-   * Добавление новой записи
-   */
-  addItem() {
-    this.setState({
-      ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
-  };
-
-  /**
-   * Удаление записи по коду
-   * @param code
-   */
-  deleteItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
-
-  /**
-   * Выделение записи по коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          item.selected = !item.selected;
+        // Возвращается функция для удаления добавленного слушателя
+        return () => {
+            this.listeners = this.listeners.filter(item => item !== listener);
         }
-        return item;
-      })
-    })
-  }
+    }
+
+    /**
+     * Выбор состояния
+     * @returns {Object}
+     */
+    getState() {
+        return this.state;
+    }
+
+    /**
+     * Установка состояния
+     * @param newState {Object}
+     */
+    setState(newState) {
+        this.state = newState;
+        // Вызываем всех слушателей
+        for (const listener of this.listeners) listener();
+    }
+
+    /**
+     * Добавление новой записи
+     */
+    addItem() {
+        const newCode = this.generateUniqueCode();
+        const newItem = {
+            code: newCode,
+            title: 'Новая запись',
+            clicked: 0
+        }
+
+        this.setState({
+            ...this.state,
+            list: [...this.state.list, newItem]
+        })
+    };
+
+    /**
+     * Удаление записи по коду
+     * @param code
+     */
+    deleteItem(code) {
+        this.setState({
+            ...this.state,
+            list: this.state.list.filter(item => item.code !== code)
+        })
+    };
+
+    /**
+     * Выделение записи по коду
+     * @param code
+     */
+    selectItem(code) {
+        this.setState({
+            ...this.state,
+            list: this.state.list.map(item => {
+                if (item.code === code) {
+                    item.selected = !item.selected;
+                    item.click++
+                } else {
+                    item.selected = false;
+                }
+
+                return item;
+            })
+        })
+    }
+
+    generateUniqueCode() {
+        let code = Math.floor(Math.random() * 10000);
+
+        while (this.usedCodes.has(code)) {
+            code = Math.floor(Math.random() * 10000);
+        }
+        this.usedCodes.add(code);
+
+        return code;
+    }
+
 }
 
 export default Store;
